41ba32d6ab104fc7d361562a5e891147900b0822
salelelie <aleishaannramos@gmail.com>
2024-02-24 23:05:59 +0800

    about/public

--- Client/src/Components/About/Types.js
+++ Client/src/Components/About/Types.js
@@ -56,22 +56,93 @@ const Types = () => {
                 </h1>
                 <p className="mb-4 pb-2">
                 Red onion is a popular vegetable in the Philippines. 
-                It is used in many dishes and is considered a staple in Filipino cuisine.Over the past few years, 
-                the price of red onions has fluctuated dramatically due to several 
-                factors including supply chain disruptions, extreme weather events, and import policies.
+                It is used in many dishes and is considered a staple in Filipino cuisine.Over the past few years,
                 the red onion industry in the Philippines faces complex challenges related to pricing, climate change, 
                 and trade policy, which require careful attention and strategic solutions to ensure the
-                 long-term sustainability of the sector and the wellbeing of local farmers
+                 long-term sustainability of the sector and the well-being of local farmers
                 </p>
               </div>
             </div>
           </div>
+        </div>
+        <div>
+      <div className="hero"></div>
+        <div className="glide" id="glide_1"></div>
+          <div className="glide__track" data-glide-el="track"></div>
+            <ul className="glide__slides"></ul>
+                <div className="center"></div>
+                  <div className="left"></div>
+                    <div className="container-fluid header bg-white p-0">
+                      <div className="row g-0 align-items-center flex-column-reverse flex-md-row">
+                        <div className="col-md-6 p-5 mt-lg-5">
+                          <h1 className="display-5 animated fadeIn mb-4">
+                            <span className="text-primary">"White Onion/ Yellow Granex " </span>
+                          </h1>
+                          <p className="animated fadeIn mb-4 pb-2">
+                          The yellow varieties grown for the traditional market are either the granex (flat) or the grano (round)
+                           type, short day onions.White onions are characterized by their milder, sweeter taste and tender texture, 
+                          making them suitable for raw applications like salads, salsas, and sandwiches. 
+                          Conversely, yellow onions offer a stronger, more robust flavor that holds its 
+                          structure well during cooking, making them ideal for sautéed onion dishes, soups,
+                           stews, and braises.
+
+                          </p>
+                        </div>
+                        <div className="col-md-5 animated fadeIn">
+                          <div className="owl-carousel header-carousel">
+                            <div className="right">
+                              <img className="img-fluid" src="../white.jpg" alt="" />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    </div>
+                    <div className="hero"></div>
+                     <div className="glide" id="glide_2">
+                      <div className="glide__track" data-glide-el="track">
+                        <ul className="glide__slides"></ul>
+                           <div className="center">
+                             <div className="right">
+        <div className="container-fluid header bg-white p-0">
+          <div className="row g-0 align-items-left flex-column-reverse flex-md-row">
+            <div className="col-md-6 p-5 mt-lg-5">
+              <div className="col-md-10 animated fadeIn">
+                <div className="owl-carousel header-carousel">
+                  <div className="left">
+                    <img className="img-fluid" src="../shallots1.jpg" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 p-5 mt-lg-5 animated fadeIn">
+              <div>
+                <h1 className="display-5 mb-4">
+                  <span className="text-primary align-items-left">"Shallots (multiplier onion)" </span>
+                </h1>
+                <p className="mb-4 pb-2">
+                Shape is one of the more distinctive differences between onions and shallots. 
+                Shallots actually come in both red and yellow skins just like onions. However, 
+                peel back those dried-up skins and you’re likely to find what looks like 
+                a narrow red onion that’s not quite as purple as a red onion usually is. The shallot is not 
+                only different in shape but also in how it grows. Like garlic, the shallot grows in clusters
+                 of two or more. That’s why when you slice a shallot in half, you can expect 
+                to find two separate halves of a bulb in what seems to be a single bulb.
+
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
   </div>
 </div>
 </div>
+</div>
+</div>
+    </div>
     </div>
   )
 }
