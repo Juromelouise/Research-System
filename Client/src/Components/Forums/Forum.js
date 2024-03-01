2b56d13010bc062b3593e6126a94f37a95cffd96
Heironymus <123807319+Juromelouise@users.noreply.github.com>
2024-03-01 20:00:42 +0800

    yeyeye

--- Client/src/Components/Forums/Forum.js
+++ Client/src/Components/Forums/Forum.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { getToken } from "../../utils/helpers";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Avatar from "@mui/material/Avatar";
@@ -15,15 +14,20 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Modal from "@mui/material/Modal";
 import { Link } from "react-router-dom";
 import { getUser } from "../../utils/helpers";
+import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
+import { Grid } from "@mui/material";
 
 const Forum = () => {
   const user = getUser();
   const [openModal, setOpenModal] = useState(false);
+  const [open, setOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [post, setPost] = useState("");
   const [forum, setForum] = useState([]);
+  const [search, setSearch] = useState("");
 
   const handleOpenModal = () => {
     setOpenModal(true);
@@ -32,6 +36,10 @@ const Forum = () => {
   const handleCloseModal = () => {
     setOpenModal(false);
   };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const newPost = async (Data) => {
     try {
       const config = {
@@ -78,14 +86,69 @@ const Forum = () => {
     handleCloseModal();
   };
 
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const buttonStyle = {
+    position: "absolute",
+    top: 120,
+    right: 10,
+  };
+
   return (
     <Fragment>
+      <>
+        <Button onClick={handleOpen} style={buttonStyle}>
+          <SearchTwoToneIcon />
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Search
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <Grid container spacing={2} alignItems="center">
+                <Grid item xs={8}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Title"
+                    variant="standard"
+                    color="secondary"
+                    fullWidth
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <Button>
+                    <SearchTwoToneIcon />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Typography>
+          </Box>
+        </Modal>
+      </>
       {forum.length > 0 ? (
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column", // Arrange items in a column
-            alignItems: "center", // Center items horizontally
+            flexDirection: "column",
+            alignItems: "center",
             gap: 2,
             marginTop: 1,
             padding: 4,
@@ -98,10 +161,10 @@ const Forum = () => {
               sx={{
                 width: 1200,
                 marginBottom: 1,
-                minHeight: 135, 
-                position: "relative", 
-                display: "flex", 
-                alignItems: "center",   
+                minHeight: 135,
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
               }}
             >
               <Avatar
@@ -126,8 +189,8 @@ const Forum = () => {
               <div
                 style={{
                   position: "absolute",
-                  bottom: 5, 
-                  right: 5, 
+                  bottom: 5,
+                  right: 5,
                 }}
               >
                 <IconButton color="primary" aria-label="Comments">
@@ -140,32 +203,34 @@ const Forum = () => {
             </Card>
           ))}
           {user ? (
-            <IconButton
-              color="primary"
-              aria-label="Open Modal"
-              onClick={handleOpenModal}
-              sx={{
-                position: "fixed",
-                bottom: 20,
-                right: 20,
-                backgroundColor: "#23BFA3",
-                "&:hover": {
-                  backgroundColor: "#00a0b2",
-                },
-                zIndex: 9999,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                padding: "10px 20px",
-                borderRadius: 4,
-                boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              <AddIcon />
-              <Typography sx={{ marginLeft: 1 }}>
-                Start a new Discussion
-              </Typography>
-            </IconButton>
+            <>
+              <IconButton
+                color="primary"
+                aria-label="Open Modal"
+                onClick={handleOpenModal}
+                sx={{
+                  position: "fixed",
+                  bottom: 20,
+                  right: 20,
+                  backgroundColor: "#23BFA3",
+                  "&:hover": {
+                    backgroundColor: "#00a0b2",
+                  },
+                  zIndex: 9999,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: "10px 20px",
+                  borderRadius: 4,
+                  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                }}
+              >
+                <AddIcon />
+                <Typography sx={{ marginLeft: 1 }}>
+                  Start a new Discussion
+                </Typography>
+              </IconButton>
+            </>
           ) : (
             <>
               <h1>login first</h1>
