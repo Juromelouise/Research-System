eff77f4e734ed2fa1fb75556ae2eef986cc11918
salelelie <aleishaannramos@gmail.com>
2024-03-05 08:59:42 +0800

    about

--- Client/src/Components/About/Problem.js
+++ Client/src/Components/About/Problem.js
@@ -73,7 +73,7 @@ const Problem = () => {
                             <div className="left">
                               <img
                                 className="img-fluid"
-                                src="../fishem.jpg"
+                                src="../red onion.jpg"
                                 alt=""
                               />
                             </div>
@@ -84,15 +84,21 @@ const Problem = () => {
                         <div>
                           <h1 className="display-5 mb-4">
                             <span className="text-primary align-items-left">
-                              "Fish Emulsion Fertilizer"{" "}
+                              "Regulator probing possible smuggling in onions being sold online"{" "}
                             </span>
                           </h1>
                           <p className="mb-4 pb-2">
-                          Fish fertilizer contains high levels of essential nutrients like nitrogen, phosphorus, potassium, 
-                          calcium, magnesium, and sulfur,all important components required by onion plants for growth.
-                          Fish fertilizers are excellent for growing onions naturally without overwhelming them with synthetic 
-                          chemicals or burning roots due to imbalanced pH levels in conventional types often encountered issues
-                           with commercial-grade products. Find yourself pleased at how much healthier your bulbs will be with this eco-friendly option!
+                          The Bureau of Plant Industry (BPI) is investigating the possibility that 
+                          onions being sold online were smuggled into the country. Import permits for 
+                          legitimate traders have been fully utilized, indicating that the online onions are 
+                          unlikely to have entered through regular channels. BPI Director Gerald Glenn F. Panganiban 
+                          stated that shipments bypassing the Sanitary and Phytosanitary Import Clearance (SPSIC) permit 
+                          and inspection process pose health risks. The BPI is coordinating with law enforcement agencies, 
+                          including the Philippine National Police, to verify the sources of the online onions. Farmers are losing 
+                          between P10,000 and P15,000 per hectare a month due to competition from smuggled onions, according to Agri 
+                          Party-list Representative Wilbert T. Lee. Onions sold online are currently being offered at P15 to P20 per 
+                          kilogram, while domestically grown red onions retail for between P60 and P120 per kilo, and imported red onions fetch P90 to P100. 
+                          Domestically grown white onions are selling for P60 to P90 per kilo, while imported varieties are offered at P80 to P120
                           </p>
                         </div>
                       </div>
@@ -114,13 +120,15 @@ const Problem = () => {
               <div className="row g-0 align-items-center flex-column-reverse flex-md-row">
                 <div className="col-md-6 p-5 mt-lg-5">
                   <h1 className="display-5 animated fadeIn mb-4">
-                    <span className="text-primary">"Compost " </span>
+                    <span className="text-primary">"Pest infestations have ravaged onion farmers’ crops" </span>
                   </h1>
                   <p className="animated fadeIn mb-4 pb-2">
-                    Compost is a rich source of organic matter and nutrients
-                    improving soil structure,drainage, and fertilizer.
-                    You can make your own compost from kitchen scraps or 
-                    yard waste.
+                  Since the arrival of the fall armyworm (Spodoptera frugiperda) in the Philippines in 2016, farmers,
+                   especially onion farmers in Bayambang, have faced significant challenges. In Bayambang, where around 
+                   1,500 hectares are dedicated to onion farming, over 800 hectares of onion plantations have been destroyed 
+                   by fall armyworm infestations, leading the town to declare a state of calamity. Farmers have resorted to excessive 
+                   use of chemical pesticides, resulting in debt and the development of pest resistance. 
+                  This ineffective cycle has trapped farmers who struggle to afford more pesticides, exacerbating their losses
                   </p>
                 </div>
                 <div className="col-md-5 animated fadeIn">
@@ -132,51 +140,7 @@ const Problem = () => {
                 </div>
               </div>
             </div>
-            <div className="hero"></div>
-            <div className="glide" id="glide_2">
-              <div className="glide__track" data-glide-el="track">
-                <ul className="glide__slides"></ul>
-                <div className="center">
-                  <div className="right">
-                    <div className="container-fluid header bg-white p-0">
-                      <div className="row g-0 align-items-left flex-column-reverse flex-md-row">
-                        <div className="col-md-6 p-5 mt-lg-5">
-                          <div className="col-md-10 animated fadeIn">
-                            <div className="owl-carousel header-carousel">
-                              <div className="left">
-                                <img
-                                  className="img-fluid"
-                                  src="../frut.jpg"
-                                  alt=""
-                                />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="col-md-6 p-5 mt-lg-5 animated fadeIn">
-                          <div>
-                            <h1 className="display-5 mb-4">
-                              <span className="text-primary align-items-left">
-                                " Fermented Fruit Juice"{" "}
-                              </span>
-                            </h1>
-                            <p className="mb-4 pb-2">
-                            Fermented Fruit Juice or FFJ for short, 
-                            is a homemade fertilizer created by fermented fruits. 
-                            Because of its potential advantages for better plant 
-                            development, fruit sweetness, and soil health, it is well-liked by organic farmers. 
-                            More research is required, even if some studies indicate it to be effective. 
-                            "Remember, it's not a replacement for a balanced fertilizer program."
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         <List component="nav">
           <Divider sx={{ my: 1 }} />
         </List>
